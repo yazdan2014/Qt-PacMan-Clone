a91036ef84a1fc15ae943ca8252bdeeb2611d573
Yaz <yazeinal@gmail.com>
2023-05-19 22:24:57 +0330

    adding builds

---
+++ FinalAP/pacman.h
@@ -0,0 +1,45 @@
+
+#ifndef PACMAN_H
+#define PACMAN_H
+
+
+#include <QObject>
+#include <QWidget>
+#include <QPoint>
+
+#include "gridpoint.h"
+
+enum Direction {
+    Left,
+    Right,
+    Up,
+    Down
+};
+
+class PacMan : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit PacMan(QWidget *parent = nullptr);
+    void moveToDestination();
+    void draw(QPainter* painter);
+    void animation_state_handler();
+
+    QPoint location;
+    Direction direction;
+    GridPoint* current_destination;
+    GridPoint* next_destination;
+    Direction last_clicked_button;
+
+    void keepGoingOrStop();
+    void nextDestinationHandler();
+
+    int movementScale = 10;
+    bool is_moving = true;
+
+
+private:
+    int animation_state;
+};
+
+#endif // PACMAN_H
